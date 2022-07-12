a8099d6fe407af28600c628a25f98f9054273e58
Antonia B. de Melo <40702662+anthello@users.noreply.github.com>
2022-07-12 17:11:34 -0300

    Create Ex.1
    Códigos dos Exercicios 1, Módulo II, Métodos Java

---
+++ MetodosJava-Ex1/Emprestimo.java
@@ -0,0 +1,34 @@
+package MetodosJava;
+
+public class Emprestimo {
+	
+	public int getDuasParcelas() {
+		return 2;
+	}	
+	public int getTresParcelas() {
+		return 3;
+	}
+	
+	public static double getTxDuasParcelas () {
+		return 0.3;
+	}
+	public static double getTxTresParcelas () {
+		return 0.45;
+	}
+	
+	public static void calcular (double valor, double d) {
+		
+		if (d == 2) {
+			double valorFinal = valor + (valor * getTxDuasParcelas());
+			System.out.println ("Valor final do empréstimo em duas parcelas: R$" + valorFinal);
+			}
+			else if (d == 3) {
+				double valorFinal = valor + (valor * getTxTresParcelas());
+				System.out.println ("Valor final do empréstimo em tres parcelas: R$" + valorFinal);
+			} else {
+				System.out.println ("Quantidade de parcelas não aceita");
+			}
+			
+	}
+}
+
