cca567978a92d5a16ad12d8f219097138442e48c
Antonia B. de Melo <40702662+anthello@users.noreply.github.com>
2022-07-13 14:48:31 -0300

    Ex2-Metodos Java
    Exercicios 2 sobre metodos Java do Bootcamp Santander DIO

---
+++ MetodosJava-Ex2/Main.java
@@ -0,0 +1,12 @@
+package MetodosJavaEx2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println ("Exercicio Quadrilátero\n");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5, 4);
+		Quadrilatero.area(5, 4, 6);
+	}
+}
